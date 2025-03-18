7af170eb59941ada7a6a8ef1f1992adc42dfddb3
rafael taparica <rafael_taparica@outlook.com>
2025-03-17 23:10:07 -0300

    new content

--- first_project/first_package/__class-11__.py
+++ first_project/first_package/__class-11__.py
@@ -6,18 +6,40 @@ to make mathematic operations and represents numerics values of any types.
 sequential type: represents string (str), lists(list), tuples(tuple).
 Dictionary type: mappings (dict)
 boolean type: true or false
-
+bool type: compare two numbers in math expression and return True or False.
 """
 
 whole = 3 # int
 floating = 3.0 # float
+#type complex
+
+
+r = complex (2,5)
+print(type(r))
+r.conjugate()
+
+w = 2 + 5j
+print(type(w))
+w.conjugate()
 
 #numeric operations
 sum_ = 1 + 2 #sum
 sub_ = 3 - 4 #subtraction
 mult_ = 4 * 7 #multiplication
 div_ = 10 / 2 #division
-sqtrt_ = 3 ** 2 #square root*
+expo_ = 3 ** 2 #exponential
+quoc_ = 21 // 2 #quocient (whole division)
+mod_ = 21 % 2 #module (rest)
+abs_ = abs(-4.73) # absolut value
+
+print("sum result is: ", sum_)
+print("subtraction result is: ", sub_)
+print("multiplication result is: ", mult_)
+print("division result is: ", div_)
+print("square root is: ", expo_)
+print("quocient is: ", quoc_)
+print("module (rest) is: ", mod_)
+print("Absolute value: ", abs_)
 
 """ 
 
@@ -30,6 +52,8 @@ print("result is: ",result)
 #boolean
 bln_f = False
 bln_t = True
+
+# for more math functions we can use the modules math and fraction.
 
 
 
