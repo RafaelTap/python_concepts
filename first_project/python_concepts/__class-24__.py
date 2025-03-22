b4a76858398c4307eebe93ee6dc45eb52ddba5ef
rafael taparica <rafael_taparica@outlook.com>
2025-03-21 21:55:51 -0300

    update

--- first_project/python_concepts/__class-24__.py
+++ first_project/python_concepts/__class-24__.py
@@ -1,4 +1,20 @@
-# repetition struct and auxiliary instruction
+# repetition struct and auxiliary instruction in python
 
 # while
+
+print("before the while block")
+
+word = input( "Enter a word: ")
+
+"""
+while word isn't 'exit', you are inside while block, 
+on the other hand, if word is 'exit", you're out of the
+block while.
+
+"""
+while word != "exit":
+    print("inside while block")
+    word = input("Enter exit to out of the while block.")
+
+print("out of the block while.")
 
