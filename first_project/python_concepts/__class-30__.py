454ca619e0b8973cb81cc149e8f72e375868ad84
rafael taparica <rafael_taparica@outlook.com>
2025-03-22 12:48:49 -0300

    update

--- first_project/python_concepts/__class-30__.py
+++ first_project/python_concepts/__class-30__.py
@@ -4,6 +4,8 @@
 
 #sequence
 
+#list
+
 list_ = [10, 20, 30, 40, 50] # list, as a vector in java.
 print("List: ", list_, "\n")
 
@@ -26,3 +28,29 @@ print(f"{third_number}")
 list_.append(60)
 
 print("List with new element: ", list_)
+
+#inserting element in a specific position, through index.
+
+list_.insert(2, 11)
+print("New element in a specific postion: ", list_,"\n")
+
+#removing element from the list: the element removed is the first founded inside the list with the same value.
+
+list_.remove(50)
+print("List without element '50': ", list_, "\n")
+
+#removing the last element of the list
+
+last_element = list_.pop() #identify the last element
+print(f'the last element:  {last_element}')
+print("Removing the last element: ", list_)
+
+#acessing a subgroup of the list
+
+sub_list = list_[1:4]
+print(f'Subgroup(index 1,2,3): {sub_list} \n')
+
+#iterating on the elements of the list
+
+for element in list_:
+    print(element)
